caa1f17e2d9bb5019369b166ea1130ddfc8e33e6
ekasunandika <ekasunandikaa@gmail.com>
2021-11-26 15:35:17 +0700

    add getjobList Search

--- backend/controllers/jobList.js
+++ backend/controllers/jobList.js
@@ -12,5 +12,21 @@ const getJobList = async (req, res) => {
     }
 }
 
-module.exports = getJobList;
+const getJobListSearch = async (req, res) => {
+    try {
+        const description = req.params.description;
+        const location = req.params.location;
+        const fulltime = req.params.fulltime || true;
+        console.log(description, location, fulltime)
+        const url = `http://dev3.dansmultipro.co.id/api/recruitment/positions.json?description=${description}&location=${location}`;
+        const jobList = await axios.get(url)
+
+        console.log(jobList.data)
+        res.json(jobList.data);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = { getJobList, getJobListSearch };
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const getJobList = require('../controllers/jobList');
+const { getJobList, getJobListSearch } = require('../controllers/jobList');
 const router = express.Router();
 
 router.get('/', getJobList);
-
+router.get('/:description&:location&:full_time', getJobListSearch);
 module.exports = router;
