ca625317ed843bd8fbbddcd5918ec871ff90fbc4
ekasunandika <ekasunandikaa@gmail.com>
2021-11-27 19:19:40 +0700

    move search to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
-import Search from './components/search';
 import './App.css';
+import "./style/search.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container } from 'react-bootstrap';
 import JobList from './components/jobList';
@@ -10,16 +10,59 @@ class App extends Component {
         super(props)
 
         this.state = {
-          jobList: []
+          jobList: [],
+          description: '',
+          location: '',
+          fulltime: false
         }
+
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleChangeLocation = this.handleChangeLocation.bind(this);
+        this.handleChangeCheckBox = this.handleChangeCheckBox.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.fetchData = this.fetchData.bind(this);
     }
 
-    async componentDidMount() {
-        const result = await fetch("http://localhost:5000/");
-        const jobs = await result.json()
-        this.setState({
-            jobList: jobs
-        });
+    
+    handleChangeDescription(event) {
+        this.setState({description: event.target.value});
+    }
+
+    handleChangeLocation(event) {
+        this.setState({location: event.target.value});
+    }
+    
+    handleChangeCheckBox(event) {
+        this.setState({fulltime: event.target.checked});
+    }
+
+    handleSearch(event) {
+        event.preventDefault();
+        
+        console.log(this.state.description);
+        console.log(this.state.location);
+        this.fetchData(this.state.description, this.state.location, this.state.fulltime)
+    }
+
+    async fetchData(description='', location='', fulltime='') {
+        if(description !== '' || location !== '' || fulltime !== '') {
+            const result = await fetch(`http://localhost:5000/${description}&${location}&${fulltime}`);
+            const jobs = await result.json()
+            this.setState({
+                jobList: jobs
+            });
+            console.log(jobs)
+        }
+        else {
+            const result = await fetch("http://localhost:5000/");
+            const jobs = await result.json()
+            this.setState({
+                jobList: jobs
+            });
+        }
+    }
+    componentDidMount() {
+        this.fetchData();
         // console.log(this.state.jobList)
     }
   
@@ -32,7 +75,31 @@ class App extends Component {
                         <Navbar.Brand href='/'><b>GitHub</b> Jobs</Navbar.Brand>
                     </Container>
                 </Navbar>
-                <Search/>
+                <div className="container">
+                    <div className="box">
+                            <label>
+                                <b>Job Description</b>
+                            </label>
+                            <input type="text" name="description" value={this.state.description} onChange={this.handleChangeDescription}/>
+                    </div>
+                    <div className="box">
+                            <label>
+                                <b>Location</b>
+                            </label>
+                            <input type="text" name="location" value={this.state.location} onChange={this.handleChangeLocation}/>
+                    </div>
+                    <div className="box">
+                        <div className="checkbox">
+                            <input type="checkbox" name="fulltime" checked={this.state.fulltime} onChange={this.handleChangeCheckBox}/>
+                            <label>
+                                <b>Full Time Only</b>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <button onClick={this.handleSearch}>Search</button>
+                    </div>
+            </div>
                 <JobList jobs={this.state.jobList}/>
             </div>
         );
--- src/components/search.js
+++ src/components/search.js
@@ -1,39 +1,70 @@
 import React, { Component } from 'react';
 import "../style/search.css"
 class Search extends Component {
-    // super(props);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: '',
+            location: '',
+            fulltime: false
+        }
+
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleChangeLocation = this.handleChangeLocation.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleChangeDescription(event) {
+        this.setState({description: event.target.value});
+    }
+
+    handleChangeLocation(event) {
+        this.setState({location: event.target.value});
+    }
+    
+
+    handleSearch(event) {
+        event.preventDefault();
+        
+        console.log(this.state.description);
+        console.log(this.state.location);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.description !== this.state.description || prevState.location !== this.state.location) {
+            console.log(this.state.description);
+            console.log(this.state.location);
+        }
+    }
 
     
     render() {
         return (
             <div className="container">
-                <div className="box">
-                    <form>
-                        <label>
-                            <b>Job Description</b>
-                        </label>
-                        <input type="text" name="description" />
-                    </form>
-                </div>
-                <div className="box">
-                    <form>
-                        <label>
-                            <b>Location</b>
-                        </label>
-                        <input type="text" name="description" />
-                    </form>
-                </div>
-                <div className="box">
-                    <form className="checkbox">
-                        <input type="checkbox" name="full-time" />
-                        <label>
-                            <b>Full-Time</b>
-                        </label>
-                    </form>
-                </div>
-                <div className="box">
-                    <button>Search</button>
-                </div>
+                    <div className="box">
+                            <label>
+                                <b>Job Description</b>
+                            </label>
+                            <input type="text" name="description" value={this.state.description} onChange={this.handleChangeDescription}/>
+                    </div>
+                    <div className="box">
+                            <label>
+                                <b>Location</b>
+                            </label>
+                            <input type="text" name="location" value={this.state.location} onChange={this.handleChangeLocation}/>
+                    </div>
+                    <div className="box">
+                        <div className="checkbox">
+                            <input type="checkbox" name="fulltime" />
+                            <label>
+                                <b>Full Time Only</b>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <button onClick={this.handleSearch}>Search</button>
+                    </div>
             </div>
         )
     }
