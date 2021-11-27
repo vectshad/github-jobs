e24f99386aa2e5dd0b39e57dc5f94780e9284e03
ekasunandika <ekasunandikaa@gmail.com>
2021-11-27 23:02:24 +0700

    add login component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import JobDetail from './components/jobDetail';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
+    <Route path='/login' element={<Login/>}/>
       <Route path='/' element={<App/>}/>
       <Route path='/detail/:id' element={<JobDetail/>}/>
     </Routes>
