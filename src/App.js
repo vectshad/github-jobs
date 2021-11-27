d7ea12c2bfa1cbeffd5cc5091d7d3575c450e4d0
ekasunandika <ekasunandikaa@gmail.com>
2021-11-27 11:19:43 +0700

    change indentation

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import { Navbar, Container } from 'react-bootstrap';
 import JobList from './components/jobList';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      jobList: []
+        this.state = {
+          jobList: []
+        }
     }
-  }
 
-  async componentDidMount() {
-    const result = await fetch("http://localhost:5000/");
-    const jobs = await result.json()
-    this.setState({
-      jobList: jobs
-    });
-    // console.log(this.state.jobList)
-  }
+    async componentDidMount() {
+        const result = await fetch("http://localhost:5000/");
+        const jobs = await result.json()
+        this.setState({
+            jobList: jobs
+        });
+        // console.log(this.state.jobList)
+    }
   
 
-  render() {
-    return (
-      <div>
-        <Navbar className="navbar" variant="dark" >
-          <Container>
-            <Navbar.Brand href="/"><b>GitHub</b> Jobs</Navbar.Brand>
-          </Container>
-        </Navbar>
-        <Search/>
-        <JobList jobs={this.state.jobList}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Navbar className='navbar' variant='dark' >
+                    <Container>
+                        <Navbar.Brand href='/'><b>GitHub</b> Jobs</Navbar.Brand>
+                    </Container>
+                </Navbar>
+                <Search/>
+                <JobList jobs={this.state.jobList}/>
+            </div>
+        );
+    }
 }
 
 export default App;
